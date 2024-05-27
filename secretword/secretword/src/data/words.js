df747b085a20af6f16954901c552c59be5a2c952
Santiago <127254370+S4nt1ag@users.noreply.github.com>
2024-05-27 19:22:21 -0300

    projeto update
    correção e refatoração de codigo

---
+++ secretword/secretword/src/data/words.js
@@ -0,0 +1,13 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Vidro", "Farol", "Buzina", "Retrovisor", "Freio", "Suspensao", "Volante", "Combustivel", "Cambio", "Bateria"],
+    fruta: ["Banana", "Maca", "Pera", "Mamao", "Laranja", "Morango", "Uva", "Abacaxi", "Kiwi", "Melancia", "Manga", "Pessego", "Cereja", "Abacate", "Goiaba"],
+    corpo: ["Braco", "Perna", "Cerebro", "Pescoco", "Olhos", "Cabelo", "Barriga", "Dedo", "Orelha", "Pe", "Cintura", "Coxa", "Joelho", "Canela", "Lingua"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memoria", "HD", "Placa-mae", "Software", "Periferico", "Ethernet", "USB", "Bluetooth", "SSD", "Firewall"],
+    programacao: ["Linguagem", "Framework", "JavaScript", "React", "Python", "HTML", "CSS", "Biblioteca", "API", "Variavel", "Compilacao", "Depuracao", "Backend", "Frontend", "Algoritmo"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo", "Legumes", "Frango", "Peixe", "Frutas", "Massa", "Pao", "Queijo", "Sopa", "Salada", "Sobremesa"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leao", "Passaro", "Girafa", "Tigre", "Tubarao", "Borboleta", "Cobra", "Macaco", "Gorila", "Panda", "Lobo", "Coelho"],
+    pais: ["Brasil", "China", "India", "Russia", "Canada", "Australia", "Japao", "Alemanha", "Franca", "Italia", "Espanha", "Mexico", "Argentina"],
+    instrumento: ["Violino", "Guitarra", "Piano", "Flauta", "Trompete", "Bateria", "Saxofone", "Violao", "Baixo", "Acordeao"],
+    profissao: ["Medico", "Professor", "Engenheiro", "Advogado", "Programador", "Artista", "Policial", "Bombeiro", "Astronomo", "Chef"],
+    esporte: ["Futebol", "Basquete", "Tenis", "Natacao", "Ciclismo", "Volei", "Atletismo", "Golfe", "Surfe", "Hoquei"]
+};
